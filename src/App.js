a51c75df70c14df159e68d0597211a612836491b
picarro-kvelayudham <kvelayudham@picarro.com>
2026-03-06 16:56:57 +0530

    feat(SCRUM-9): Fetched via MCP [ScopeSentinel]

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ToDoComponent from './components/ToDoComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <ToDoComponent />
+    </div>
+  );
+}
+
+export default App;
